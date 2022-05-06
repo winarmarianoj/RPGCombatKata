0c40c1f000cfa4c045d59965b919c768377393d4
Mariano Wiñar <50705701+winarmarianoj@users.noreply.github.com>
2022-05-05 22:08:10 -0300

    Iteration One and First Item

---
+++ Assets/Scripts/Domain/Character.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain
+{
+    public class Character
+    {
+        public int hp;
+        public int level;
+        public bool isAlive;
+        private const int STARTING_LEVEL = 1;
+        private const int STARTING_HP = 1000;
+        public Character()
+        {
+            hp = STARTING_HP;
+            level = STARTING_LEVEL;
+            isAlive = true;
+        }
+    }
+}
--- Assets/Scripts/Tests/CharacterShould.cs
+++ Assets/Scripts/Tests/CharacterShould.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Domain;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -8,6 +9,36 @@ namespace Tests
 {
     public class CharacterShould
     {
-        
+        private Character _character;
+
+        [SetUp]
+        public void Setup()
+        {
+            _character = new Character();
+        }
+
+        [Test]
+        public void StartWith1000hp()
+        {
+            int result = _character.hp;
+            
+            Assert.AreEqual(1000, result);
+        }
+
+        [Test]
+        public void StartLevelOne()
+        {
+            int result = _character.level;
+            
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void StartAlive()
+        {
+            bool result = _character.isAlive;
+
+            Assert.IsTrue(result); 
+        }
     }
 }
